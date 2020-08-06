bec1d92da3fc5f97b1ae001fff5c524fbdfda326
david <ddawkins417@gmail.com>
2020-08-06 15:36:54 -0500

    add test

---
+++ DevopsCI.Testing/Tests/HomeTest.cs
@@ -0,0 +1,17 @@
+using DevopsCI.Client.Controllers;
+using Xunit;
+
+namespace DevopsCI.Testing.Tests
+{
+  public class HomeTest
+  {
+    [Fact]
+    public void HomeController_Test()
+    {
+      var sut = new HomeController();
+      var view = sut.Index();
+
+      Assert.NotNull(view);
+    }
+  }
+}
